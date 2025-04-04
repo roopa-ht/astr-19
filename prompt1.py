6da16a379bbc64e65e8cfa7c12738c863efff579
roopa-ht <roopahope@icloud.com>
2025-04-03 18:19:34 -0700

    Adding Code Journal 1 Prompt 1

---
+++ prompt1.py
@@ -0,0 +1,8 @@
+'''
+Write a Python program to print out your full name
+Your preferred pronouns (optional)
+Two sentences about your favorite movie and your favorite food.
+'''
+
+print("Hi, my name is Roopa Thiagaraj and my preferred pronouns are she/they.")
+print("My favorite movie is Lights Out because I love horror. \nMy favorite food is Biriyani because it is a food my mom makes from my culture!")
